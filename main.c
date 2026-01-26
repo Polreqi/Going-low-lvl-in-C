7c7359a51a5044c8ded668acbb6e36ac868b2f20
Anng3l <230401972+Polreqi@users.noreply.github.com>
2026-01-25 20:43:42 -0500

    fc

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h> //preprocessor directive
+
+int main(){
+
+    // Para imprimir, debe ir entre comillas dobles
+    printf("1\n");
+    printf("33\n");
+    
+    int edad = 30;
+
+    int year = 22;
+    float presion = 9.5;
+
+    printf("El year es el %d, y el float es: %-3.1f", year, presion);
+    
+    return 0;
+}
