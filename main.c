96986eea35ab1fd424e6767c4c647b9c58aceb9c
Anng3l <230401972+Polreqi@users.noreply.github.com>
2026-01-29 21:32:30 -0500

    Estudiando 2

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main(){
 
+    /*
+    
+        Int y Float
+
+    */
+
     // Para imprimir, debe ir entre comillas dobles
     printf("1\n");
     printf("33\n");
@@ -9,9 +15,56 @@ int main(){
     int edad = 30;
 
     int year = 22;
-    float presion = 9.5;
+    float presion = 92.534;
 
-    printf("El year es el %d, y el float es: %-3.1f", year, presion);
+                                            // El '-' es una flag, indica la alineación (izquierda)
+                                            // '3' indica el número de caracteres que se
+                                            // mostrarán del argumento que se pase.
+                                            // '.1' indica el número de decimales que se mostrarán.
+                                            // 'f' indica el tipo de dato (float)
+    //printf("El year es el %d, y el float es: %-7.1f ssss", year, presion);
     
-    return 0;
+
+    
+    
+    
+    
+    /*
+    
+        Double
+    
+    */
+    double e = 2.71828182842904523536;
+
+                        // Long floating point number
+    printf("Numero euler: %.20lf\n", e);
+
+    
+
+
+
+
+
+    /*
+    
+        Char & String
+
+    */
+   char axle = 'a';
+   char glimmer = 'g';
+
+   printf("Caracteres:\n1. %c\n2. %c\n", axle, glimmer);
+
+
+   char usaCurrency = '$';
+   char percentage = '%';
+   printf("Los simbolos son: '%c' y '%c'\n", usaCurrency, percentage);
+   
+   
+   
+    char phrase[] = 'Probando tipos de datos en C';
+    printf("La frase es: %s", phrase);
+
+
+   return 0;
 }
